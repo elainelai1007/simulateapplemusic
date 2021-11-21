b92c23b1776beba050a52c0ba4ce3791a0f186ab
Elaine <elaine@musedp.com>
2021-11-21 15:14:54 +0800

    Initial Commit

---
+++ simulateapplemusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simulateapplemusic
+//
+//  Created by Elaine Lai on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
